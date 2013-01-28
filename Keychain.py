97b3f16fad8ad28407f8c7acee4e3d0997edd593
Daniel Shannon <d@daniel.sh>
2013-01-28 04:16:03 -0500

    Keychain.modifyPassword() fixes
    Fixed ctypes memory issues with Keychain.modifyPassword() that were
    causing massive total existence failure.

--- Keychain.py
+++ Keychain.py
@@ -19,7 +19,7 @@ class Keychain:
         pwLen = c_ulong(len(password))
         acctData = c_char_p(account)
         pwData = c_char_p(password)
-        status = security.SecKeychainAddGenericPassword(
+        security.SecKeychainAddGenericPassword(
                     None,
                     self.serviceLen,
                     self.service,
@@ -29,13 +29,12 @@ class Keychain:
                     pwData,
                     None
             )
-        return status
 
     def retrievePassword(self, account):
         acctLen = c_ulong(len(account))
         acctData = c_char_p(account)
         pwLen = c_ulong()
-        pwData = c_char_p()
+        pwData = pointer(c_char_p())
 
         security.SecKeychainFindGenericPassword(
                     None,
@@ -44,37 +43,46 @@ class Keychain:
                     acctLen,
                     acctData,
                     byref(pwLen),
-                    byref(pwData),
+                    pwData,
                     None
             )
 
-        return pwData.value
+        return pwData[0]
 
     def modifyPassword(self, account, newPassword):
         acctLen = c_ulong(len(account))
-        newPwLen = c_ulong(len(newPassword))
-        itemRef = c_void_p()
-        pwLen = c_ulong()
-        pwData = c_char_p()
-        newPwData = c_char_p(newPassword)
+        newPwLen = c_ulong(len(newPassword) + 1)
+        itemRef = pointer(c_void_p())
+        newPwData = create_string_buffer(newPassword)
 
-        status = security.SecKeychainFindGenericPassword(
+        security.SecKeychainFindGenericPassword(
                     None,
                     self.serviceLen,
                     self.service,
                     acctLen,
                     account,
-                    byref(pwLen),
-                    byref(pwData),
+                    None,
+                    None,
+                    itemRef
+            )
+        security.SecKeychainItemModifyAttributesAndData(
+                itemRef.contents,
+                None,
+                newPwLen,
+                newPwData
+        )
+
+    def deletePassword(self, account):
+        acctLen = c_ulong(len(account))
+        itemRef = c_void_p()
+        security.SecKeychainFindGenericPassword(
+                    None,
+                    self.serviceLen,
+                    self.service,
+                    acctLen,
+                    account,
+                    None,
+                    None,
                     byref(itemRef)
             )
-        if status == 0:
-            modStatus = security.SecKeychainItemModifyAttributesAndData(
-                    itemRef,
-                    None,
-                    newPwLen,
-                    newPwData
-            )
-            return modStatus
-        else:
-            return None
+        security.SecKeychainItemDelete(itemRef)
