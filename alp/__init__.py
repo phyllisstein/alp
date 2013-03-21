3ae0bec1233f95749e7ab67de48055ffd27f3b6e
Daniel Shannon <d@daniel.sh>
2013-03-21 00:50:28 -0400

    Notification imported separately (addresses #6)

--- alp/__init__.py
+++ alp/__init__.py
@@ -3,8 +3,11 @@ try:
     from .item import *
     from .keychain import *
     from .settings import *
-    from .notification import *
     from .mail import *
     from alp.request.request import *
+except ImportError:
+    pass
+try:
+    from .notification import *
 except ImportError:
     pass
